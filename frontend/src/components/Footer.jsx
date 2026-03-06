d8bbf5946636cd08a73a68993fa12337a5a38c23
Verónica <vero7nicaxs@gmail.com>
2026-03-07 00:16:30 +0100

    Se agrega pie de página "Footer" (CSS y JSX)

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import "../styles/Footer.css"
+
+function Footer() {
+    return (
+        <div  >
+            {/* Pie de página */}
+            <footer className="footer-cuadrado">
+                &copy; Hestia: Hotel Management System (HMS) 
+            </footer>
+        </div>
+    );
+}
+export default Footer;
