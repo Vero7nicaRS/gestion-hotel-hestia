fc6758d9e0edf58cdca8346d94ce26a8ebebccfc
Verónica <vero7nicaxs@gmail.com>
2026-03-07 17:31:56 +0100

    Actualización "main.jsx"

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import HabitacionDetalle from './pages/HabitacionDetalle.jsx'
-import NavBar from './components/NavBar.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
